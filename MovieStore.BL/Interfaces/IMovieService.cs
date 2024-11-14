1786451c1bbaddd390d5d7b139d261f085cfe532
Ivan Yordanov <55908650+ivo0509@users.noreply.github.com>
2024-11-14 18:21:51 +0200

    Add files via upload

---
+++ MovieStore.BL/Interfaces/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using MovieStore.Models.DTO;
+
+namespace MovieStore.BL.Interfaces
+{
+    public interface IMovieService
+    {
+        List<Movie> GetAllMovies();
+
+        void AddMovie(Movie movie);
+    }
+}
